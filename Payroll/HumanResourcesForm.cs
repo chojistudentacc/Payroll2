884b15b4d76ea2b59ad2273accac9df283d15d1a
NotNookie <lhexuspaul@gmail.com>
2025-12-08 19:48:43 +0800

    HR Form started. Kain muna ako man

---
+++ Payroll/HumanResourcesForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll
+{
+    public partial class HumanResourcesForm : Form
+    {
+        public HumanResourcesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void HumanResourcesForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+        }
+    }
+}
